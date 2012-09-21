5a382983997fcd81e841cd844f8ddd6738fd221d
vit.tam@gmail.com <vit.tam@gmail.com>
2012-09-22 01:18:04 +0300

    ticket:1 added selenium tests

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -1,22 +1,6 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
-
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
 
 
 class HttpTest(TestCase):
@@ -24,4 +8,14 @@ class HttpTest(TestCase):
         c = Client()
         response = c.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hello!')
+        self.assertContains(response, '42 Coffee Cups Test Assignment')
+
+
+class BaseSeleniumTest(TestCase):
+    def test_simple(self):
+        from selenium import webdriver
+
+        browser = webdriver.Firefox() # Get local session of firefox
+        browser.get('http://0.0.0.0:8000') # Load page
+        assert "Hello, 42cc!!!" in browser.title
+        browser.close()
