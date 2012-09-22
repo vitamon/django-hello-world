371a6b282f252ede13083f5269a89d52debb89b9
vitamon <vit.tam@gmail.com>
2012-09-22 21:39:08 +0300

    t5: updates in templeates

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -6,13 +6,14 @@ from django.contrib.auth import logout
 
 @render_to('hello/home.html')
 def home(request):
-    return {'user':request.user}
+    return {'user': request.user}
 
 
 @render_to('hello/requests.html')
 def requests(request):
     top_ten = RequestsLog.objects.get_last_ten()
     return {"items": top_ten}
+
 
 def logout_view(request):
     logout(request)
