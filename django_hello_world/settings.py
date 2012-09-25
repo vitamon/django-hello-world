216ef5475a8bbaf749335a3d08385038f4618ba1
vitamon <vit.tam@gmail.com>
2012-09-26 00:11:51 +0300

    fix for build path

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -1,7 +1,10 @@
 # Django settings for django_hello_world project.
+import sys
 import os
 
 PROJECT_ROOT    = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, PROJECT_ROOT)
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
